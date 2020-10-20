1d5dd4967783a9ceffe1dcf968f41ec4b0880ea7
Cassandra Liau <cass@Cassandras-MacBook-Pro.local>
2020-10-21 04:02:48 +0800

    News API added

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Home() {
+  const [newsData, setNewsData] = useState([]);
+  const [gifs, setGifs] = useState([]);
+  const [term, updateTerm] = useState("heart");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?q=trump&apiKey=548f1408238c4a3d8013767237f2c84d`
+      )
+      .then(function (response) {
+        const articleDataResponse = response.data.articles;
+        setNewsData(articleDataResponse);
+        console.log(`article data response`, articleDataResponse);
+      })
+      .catch(function (error) {
+        console.warn(error);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Midterm</h1>
+      <h1>Home</h1>
+      {newsData.map((articles, i) => (
+        <div key={i}>
+          <h3>Article Title: </h3>
+          <p>{articles.title}</p>
+        </div>
+      ))}
     </div>
   );
 }
