319cae40c3b88d43f5fac9d27c54fd46438552e6
Cassandra Liau <cass@Cassandras-MacBook-Pro.local>
2020-10-26 06:22:34 +0800

    two apis added

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,20 +1,36 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+// const NEWSAPI = process.env.REACT_APP_NEWS_API_KEY;
+// console.log(`api key`, NEWSAPI);
+
 function Home() {
+  const [category, setCategory] = useState("bitcoin");
   const [newsData, setNewsData] = useState([]);
-  const [gifs, setGifs] = useState([]);
-  const [term, updateTerm] = useState("heart");
+  const [memeData, setMeme] = useState([]);
 
   useEffect(() => {
     axios
       .get(
-        `https://newsapi.org/v2/top-headlines?q=trump&apiKey=548f1408238c4a3d8013767237f2c84d`
+        `https://newsapi.org/v2/everything?q=${category}&apiKey=548f1408238c4a3d8013767237f2c84d`
       )
       .then(function (response) {
         const articleDataResponse = response.data.articles;
         setNewsData(articleDataResponse);
         console.log(`article data response`, articleDataResponse);
+      })
+      .catch(function (error) {
+        console.warn(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.imgflip.com/get_memes`)
+      .then(function (response) {
+        const memeDataResponse = response.data.data.memes;
+        setMeme(memeDataResponse);
+        console.log("meme data response", memeDataResponse);
       })
       .catch(function (error) {
         console.warn(error);
@@ -28,6 +44,12 @@ function Home() {
         <div key={i}>
           <h3>Article Title: </h3>
           <p>{articles.title}</p>
+        </div>
+      ))}
+      {memeData.map((memes, i) => (
+        <div key={i}>
+          <h3>Meme: </h3>
+          <img src={memes.url} alt=""></img>
         </div>
       ))}
     </div>
